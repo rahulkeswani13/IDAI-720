4712ee37c108b18c1b36420bc1139529e0e2523b
“rahulkeswani13” <rahulskeswani001@gmail.com>
2025-04-01 16:13:01 -0400

    Updated metrics.py file for A3

--- assignments/src/metrics.py
+++ assignments/src/metrics.py
@@ -16,6 +16,18 @@ class Metrics:
         # return: EOD = TPR(s=1)-TPR(s=0)
         # TPR = #(y=1, y_pred=1) / #(y=1)
         # Write your code below:
+        
+        # Convert y and y_pred to numpy arrays for easier manipulation
+        y = np.array(self.y)
+        y_pred = np.array(self.y_pred)
+
+        # Mask for sensitive attribute s=1
+        mask_s1 = np.array(s) == 1
+        tpr1 = np.sum((y == 1) & (y_pred == 1) & mask_s1) / np.sum((y == 1) & mask_s1)
+
+        # Mask for sensitive attribute s=0
+        mask_s0 = np.array(s) == 0
+        tpr0 = np.sum((y == 1) & (y_pred == 1) & mask_s0) / np.sum((y == 1) & mask_s0)
 
         return tpr1-tpr0
 
@@ -26,6 +38,19 @@ class Metrics:
         # TPR = #(y=1, y_pred=1) / #(y=1)
         # FPR = #(y=0, y_pred=1) / #(y=0)
         # Write your code below:
+        # Convert y and y_pred to numpy arrays for easier manipulation
+        y = np.array(self.y)
+        y_pred = np.array(self.y_pred)
+
+        # Mask for sensitive attribute s=1
+        mask_s1 = np.array(s) == 1
+        tpr1 = np.sum((y == 1) & (y_pred == 1) & mask_s1) / np.sum((y == 1) & mask_s1)
+        fpr1 = np.sum((y == 0) & (y_pred == 1) & mask_s1) / np.sum((y == 0) & mask_s1)
+
+        # Mask for sensitive attribute s=0
+        mask_s0 = np.array(s) == 0
+        tpr0 = np.sum((y == 1) & (y_pred == 1) & mask_s0) / np.sum((y == 1) & mask_s0)
+        fpr0 = np.sum((y == 0) & (y_pred == 1) & mask_s0) / np.sum((y == 0) & mask_s0)
 
         return (tpr1 - tpr0 + fpr1 - fpr0)/2.0
 
@@ -35,6 +60,16 @@ class Metrics:
         # return: SPD = |PR(s=1)-PR(s=0)|
         # PR = #(y_pred=1) / #y
         # Write your code below:
+        # Convert y_pred to a numpy array for easier manipulation
+        y_pred = np.array(self.y_pred)
+
+        # Mask for sensitive attribute s=1
+        mask_s1 = np.array(s) == 1
+        pr1 = np.sum(y_pred[mask_s1] == 1) / np.sum(mask_s1)
+
+        # Mask for sensitive attribute s=0
+        mask_s0 = np.array(s) == 0
+        pr0 = np.sum(y_pred[mask_s0] == 1) / np.sum(mask_s0)
 
         return pr1 - pr0
 
